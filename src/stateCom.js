8f0c1bdc93938468e11ac709fdb6b41e4594fe8d
unknown <rajeshnalleboina94@gmail.com>
2020-09-13 18:57:00 +0530

    first

---
+++ src/stateCom.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class StateComponent extends Component {
+  render() {
+    return <h1>It's a State Component</h1>;
+  }
+}
+
+export default StateComponent;
